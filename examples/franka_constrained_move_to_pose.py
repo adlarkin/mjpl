382e1e311e773cdf46566b1412f7db4a46f1b7fd
Ashton Larkin <42042756+adlarkin@users.noreply.github.com>
2025-05-08 17:16:08 -0600

    one more: use generate_constrained_trajectory in franka pose constraint example

--- examples/franka_constrained_move_to_pose.py
+++ examples/franka_constrained_move_to_pose.py
@@ -102,7 +102,9 @@ def main():
 
     print("Generating trajectory...")
     start = time.time()
-    trajectory = traj_generator.generate_trajectory(shortcut_waypoints)
+    trajectory = mjpl.generate_constrained_trajectory(
+        shortcut_waypoints, traj_generator, constraints
+    )
     print(f"Trajectory generation took {(time.time() - start):.4f}s")
 
     # Actuator indices in data.ctrl that correspond to the joints in the trajectory.
